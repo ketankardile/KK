3b1a2cbdd6184068b5ff1a911c7dbf0f2cbfcbed
KETAN KARDILE <ketankardile25@gmail.com>
2021-10-09 01:22:45 +0530

    ok
    ok

--- gitproject/src/gitproject/ClassA.java
+++ gitproject/src/gitproject/ClassA.java
@@ -5,7 +5,7 @@ public class ClassA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("ketguuigigui");
+		System.out.println("ketguuigigui mbjb");
 		
 	}
 
